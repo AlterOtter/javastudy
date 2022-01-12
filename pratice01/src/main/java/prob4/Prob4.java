9822ae752daac79f9105f59c307bd9685d698742
AlterOtter <oksonsango2@gmail.com>
2022-01-12 19:27:27 +0900

    subject

--- pratice01/src/main/java/prob4/Prob4.java
+++ pratice01/src/main/java/prob4/Prob4.java
@@ -9,7 +9,15 @@ public class Prob4 {
 
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
-
+		if(text!=null) {
+			char[] arr=text.toCharArray();
+			for(int i=0;i<arr.length;i++) {
+				for(int j=0;j<i;j++) {
+					System.out.print(arr[j]);
+				}
+				System.out.println();
+			}
+		}
 		
 		scanner.close();
 	}
